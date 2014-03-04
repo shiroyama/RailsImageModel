5cc393fbcdfacefe163a9528ea2a5ba254321771
Fumihiko Shiroyama <fu.shiroyama@gmail.com>
2014-03-04 08:44:01 +0000

    Let environment know where the ImageMagick path is

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ ImageModel::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # tells paperclip where imagemagick is
+  Paperclip.options[:command_path] = "/usr/bin/"
 end
